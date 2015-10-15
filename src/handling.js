b0e35fd27a0fd8dff6d7b3660a8bf02001385627
Kalenvs <KalenvanStaden@Gmail.com>
2015-10-15 11:45:32 -0700

    Added handling.js and psuedo code functionality to login and signout elements.

---
+++ src/handling.js
@@ -0,0 +1,9 @@
+function login(){
+	document.getElementById("navi").style.visibility = "visible";
+	document.getElementById("post").style.visibility = "visible";
+	document.getElementById("login").style.visibility = "hidden";
+};
+
+function logout(){
+	location.reload();
+};
