6cd93f4cf28a3b17d4003e786ce4237d5f64f086
Rica Feng <topgb@msn.com>
2015-10-16 15:11:15 -0700

    function for comparing passwords

--- src/login.php
+++ src/login.php
@@ -3,13 +3,19 @@
 $password = $login = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $login = test_input($_POST["username"]);
-   $password = test_input($_POST["password"]);
+   $login = test_input($_POST["userIn"]);
+   $password = test_input($_POST["passIn"]);
+}
 
 function test_input($data) {
    $data = trim($data);
    $data = stripslashes($data);
    $data = htmlspecialchars($data);
    return $data;
+}
+
+//returns true if passwords match
+function checkpassword($pass, $passvalid) {
+	return ($pass == $passvalid);
 }
 ?>
