afdf3838a32cfd4a9e4b0c2c68857e3914f2671c
Kalenvs <KalenvanStaden@Gmail.com>
2015-10-16 00:20:03 -0700

    added account creation html + modified .js file to handle

--- src/handling.js
+++ src/handling.js
@@ -1,9 +1,23 @@
 function login(){
 	document.getElementById("navi").style.visibility = "visible";
 	document.getElementById("post").style.visibility = "visible";
+	document.getElementById("createAcc").style.visibility = "hidden";
 	document.getElementById("login").style.visibility = "hidden";
 };
 
 function logout(){
 	location.reload();
 };
+
+
+function createAcc(){
+	document.getElementById("navi").style.visibility = "hidden";
+	document.getElementById("post").style.visibility = "hidden";
+	document.getElementById("createAcc").style.visibility = "visible";
+	document.getElementById("login").style.visibility = "hidden";
+}
+
+function verifyAcc(){
+	
+	
+}
