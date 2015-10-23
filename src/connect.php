599ca6ddd8a0c148276599bfe46242c72225f5c6
johnli1101 <jli042@ucr.edu>
2015-10-23 13:17:30 -0700

    Adjustments to Connect, made a 2nd login.php, and authentication.
    Added a database connect for connect.php, added the authentication.php
    to connect with the login. Have a 2nd login.php to try new things for
    the login.

--- src/connect.php
+++ src/connect.php
@@ -6,10 +6,16 @@ $password = "";
 //what database to search through
 $dbname = "testrumblr";
 
+//added for log in to connect to the mySQL database (John)
+$bd = mysql_connect($servername, $username, $password) 
+	or die("Could not connect database");
+	mysql_select_db($dbname, $bd) or die("Could not select database");
+
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
+
   return $data;
 }
 
