df6bda578521210d79711d8cb2335875b02b02f8
dcsmitty <dcsmitty12@yahoo.com>
2015-11-05 11:57:57 -0800

    Added Post Table to the Database

--- src/rumblr.sql
+++ src/rumblr.sql
@@ -2,8 +2,8 @@
 -- version 4.4.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 05, 2015 at 10:37 AM
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2015 at 08:56 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `testrumblr`
+-- Database: `rumblr`
 --
 
 -- --------------------------------------------------------
@@ -30,16 +30,21 @@ CREATE TABLE IF NOT EXISTS `login` (
   `loginID` int(11) NOT NULL,
   `login` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `login`
+-- Table structure for table `posts`
 --
 
-INSERT INTO `login` (`loginID`, `login`, `password`) VALUES
-(27, 'pop', 'pop'),
-(28, 'pop', 'pop'),
-(29, 'sdjfhq', 'pop');
+CREATE TABLE IF NOT EXISTS `posts` (
+  `Post_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Post_Type` int(11) NOT NULL,
+  `Post` text NOT NULL,
+  `Time_Stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,16 +64,7 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `interests` text,
   `blogdesc` text,
   `username` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `profile`
---
-
-INSERT INTO `profile` (`profileID`, `lname`, `fname`, `nickname`, `gender`, `birthday`, `profilecreation`, `photo`, `interests`, `blogdesc`, `username`) VALUES
-(27, 'jknsdf', 'poudfj', NULL, 'Male', '2015-11-20', '2015-11-05 08:37:37', NULL, NULL, NULL, ''),
-(28, 'pop', 'pop', NULL, 'Male', '2015-11-10', '2015-11-05 08:37:49', NULL, NULL, NULL, ''),
-(29, 'ad', 'pop', NULL, 'Male', '2015-11-25', '2015-11-05 08:39:10', NULL, NULL, NULL, 'isjodfsd');
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -79,6 +75,12 @@ INSERT INTO `profile` (`profileID`, `lname`, `fname`, `nickname`, `gender`, `bir
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`loginID`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`Post_ID`);
 
 --
 -- Indexes for table `profile`
@@ -94,12 +96,17 @@ ALTER TABLE `profile`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `Post_ID` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profileID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+  MODIFY `profileID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
