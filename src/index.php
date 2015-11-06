53a5d31cd0cdb30e674494e81c83e98a4d57a803
johnli1101 <jli042@ucr.edu>
2015-11-05 22:11:56 -0800

    changed login and index changed login and added top

--- src/index.php
+++ src/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
+<script src="handling.js"></script>
+<?php
+	include('login.php');
+
+	if(	isset($_SESSION['SESS_USERNAME']))
+	{
+		echo "I'm In";
+		echo "<script src='handling.'>hide(2);</script>";
+		session_destroy();
+	}
+	else
+	{
+		echo session_status();
+	}
+?>
 <html>
 <body style="background-color:DarkCyan">
 	<head> 
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<script src="handling.js"></script>
-		<?php include 'connect.php';?>
+		<?php //include 'connect.php';?>
 		<?php include 'createacc.php';?>
+		<?php //include 'login.php';?>
 	</head>
 	<div id = "header">
 		<header id = "title">
@@ -32,15 +48,15 @@
 	
 	<div id = "Body">
 		<div id = "postBar">
-			<a onclick = "post(0)">Text</a><br>
+			<a onclick = "posts()">Text</a><br>
 
 			<a>Photo </a><br>
 
-			<a onclick = "post(2)">Quote </a><br>
+			<a onclick = "postQuote()">Quote </a><br>
 			
-			<a onclick = "post(3)">Link</a><br>
+			<a onclick = "postLink()">Link</a><br>
 
-			<a onclick = "post(4)">Chat </a><br>
+			<a onclick = "postChat()">Chat </a><br>
 
 			<a>Audio </a><br>
 			<a>Video </a><br>
@@ -48,15 +64,60 @@
 			<a onclick = "logout()" >Sign out?</a>
 		</div>
 
-		<div id = "login">
+
+
+
+		<div id = "quote">
 			<form>
+				Create QUOTE post:<br>
+				<input type="text" style="font-size:12pt;height:120px;width:200px;">
+				<br><br>
+				<input type="submit" value = "POST!">
+
+			</form>
+		</div>
+
+		<div id = "posts">
+			<form>
+				Create TEXT post:<br>
+				<input type="text" style="font-size:12pt;height:120px;width:200px;">
+				<br><br>
+				<input type="Submit" value = "POST!">
+
+			</form>
+		</div>
+
+		<div id = "link">
+			<form>
+				Create LINK post:<br>
+				<input type = "text" name="type here" size = "50">
+				<br><br>
+				<input type="Submit" value = "POST!">
+
+			</form>
+		</div>
+
+		<div id = "chat">
+			<form>
+				Create CHAT post:<br>
+				<input type="text" style="font-size:12pt;height:220px;width:200px;">
+				<br><br>
+				<input type="Submit" value = "POST!">
+
+			</form>
+		</div>
+
+
+		
+		<div id = "login">
+			<form method="post" action="login.php">
 				Username:<br>
 				<input type="text" id = "userIn" name = "login"/>
 				<br>
 				Password: <br>
 				<input type="password" id = "passIn" name = "password"/>
 				<br> <br>
-				<input type ="button" onclick = "hide(2)" Value = "Login"/>
+				<input type ="submit" onclick = "hide(2)" Value = "Login" name = "verifiedLogin"/>
 				<input type ="button" onclick = "hide(1)" Value = "Create Account"/>
 			</form>
 		</div>
@@ -112,48 +173,6 @@
 		</div>
 		
 		<div id = "posts"></div>
-		
-		<div id = "posting">
-
-			<div id = "quote">
-				<form>
-					Create QUOTE post:<br>
-					<input type="text" style="font-size:12pt;height:120px;width:200px;" name = "quote_enter">
-					<br><br>
-					<input type="submit" value = "POST!" name =  "quote_sub">
-
-				</form>
-			</div>
-
-			<div id = "text">
-				<form>
-					Create TEXT post:<br>
-					<input type="text" style="font-size:12pt;height:120px;width:200px;" name = "text_enter">
-					<br><br>
-					<input type="submit" value = "POST!" name = "text_sub">
-				</form>
-			</div>
-
-			<div id = "link">
-				<form>
-					Create LINK post:<br>
-					<input type = "text" name="type here" size = "50" name = "link_enter">
-					<br><br>
-					<input type="submit" value = "POST!" name = "link_sub">
-
-				</form>
-			</div>
-
-			<div id = "chat">
-				<form>
-					Create CHAT post:<br>
-					<input type="text" style="font-size:12pt;height:220px;width:200px;" name ="chat_enter">
-					<br><br>
-					<input type="submit" value = "POST!" name = "chat_sub">
-
-				</form>
-			</div>
-		</div>
 		
 		<div id="profile">
 			<img id="profPic" src ="http://freethoughtblogs.com/lousycanuck/files/2014/05/hqdefault.jpg"/>
--- src/login.php
+++ src/login.php
@@ -1,21 +1,93 @@
 <?php
 // define variables and set to empty values
-$password = $login = "";
+$password1 = $login1 =  "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $login = test_input($_POST["login"]);
-   $password = test_input($_POST["password"]);
+session_start();
+
+//connect to the database
+require_once('connect.php');
+
+//WHY?!
+function test_input1($data) 
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+
+  return $data;
 }
 
-function test_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
+//if logged in then do stuff
+if(isset($_POST['verifiedLogin'])) 
+{
+	//check for login from html if not then return an error
+	if (empty($_POST["login"])) 
+	{
+		echo "login1";
+		$err_main += "A username is required! <br>";
+		header("Location:index.php");
+	} 
+	else 
+	{
+		$login1 = test_input1($_POST["login"]);
+		echo "I' passed login";
+	}
+
+	//check for password from html if not then return an error
+	if (empty($_POST["password"])) 
+	{
+		echo "login2";
+		$err_main += "A password is required! <br>";
+		header("Location:index.php");
+	} 
+	else 
+	{
+		$password1 = test_input1($_POST["password"]);
+	}
+
+	$qry="SELECT * FROM login WHERE login='$login1' AND password='$password1'";
+	$result = mysql_query($qry);
+
+	if($result) 
+	{
+		if(mysql_num_rows($result) > 0) 
+		{
+			//Login Successful
+			//session_regenerate_id();
+			$member = mysql_fetch_assoc($result);
+			$_SESSION['SESS_LOGIN_ID'] = $member['loginid'];
+			$_SESSION['SESS_USERNAME'] = $member['login'];
+			$_SESSION['SESS_PASSWORD'] = $member['password'];
+			//session_write_close();
+
+			header("location: index.php");
+
+			exit();
+		}
+		else 
+		{
+			//Login failed
+			echo "user name and info";
+			//$errmsg_arr[] = 'user name and password not found';
+			$errflag = true;
+			if($errflag) 
+			{
+				$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+				session_write_close();
+				header("location: index.php");
+				exit();
+			}
+		}
+	}
+	else 
+	{
+		die("Query failed");
+	}
 }
 
 //returns true if passwords match
-function checkpassword($pass, $passvalid) {
+function checkpassword($pass, $passvalid) 
+{
 	return ($pass == $passvalid);
 }
 ?>
