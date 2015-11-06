bfab45ce6963d52c1ca6cc54354e865dd95f35f2
Rica Feng <topgb@msn.com>
2015-11-05 20:01:45 -0800

    added posts table

--- src/rumblr.sql
+++ src/rumblr.sql
@@ -2,8 +2,8 @@
 -- version 4.4.14
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 05, 2015 at 08:56 PM
+-- Host: localhost
+-- Generation Time: Nov 06, 2015 at 05:01 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -23,90 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
---
-
-CREATE TABLE IF NOT EXISTS `login` (
-  `loginID` int(11) NOT NULL,
-  `login` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `posts`
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `Post_ID` int(11) NOT NULL,
-  `User_ID` int(11) NOT NULL,
-  `Post_Type` int(11) NOT NULL,
-  `Post` text NOT NULL,
-  `Time_Stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `postID` int(11) unsigned NOT NULL,
+  `type` int(5) unsigned NOT NULL,
+  `info` varchar(12) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `profile`
---
-
-CREATE TABLE IF NOT EXISTS `profile` (
-  `profileID` int(11) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `fname` varchar(255) NOT NULL,
-  `nickname` varchar(255) DEFAULT NULL,
-  `gender` varchar(11) NOT NULL,
-  `birthday` date DEFAULT NULL,
-  `profilecreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `photo` blob,
-  `interests` text,
-  `blogdesc` text,
-  `username` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`loginID`);
-
---
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`Post_ID`);
+  ADD PRIMARY KEY (`postID`,`type`,`info`,`timestamp`);
 
---
--- Indexes for table `profile`
---
-ALTER TABLE `profile`
-  ADD PRIMARY KEY (`profileID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `login`
---
-ALTER TABLE `login`
-  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `Post_ID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `profile`
---
-ALTER TABLE `profile`
-  MODIFY `profileID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
