9c591ab88a6adbb165c5a1534598eadd50292b49
Rica Feng <topgb@msn.com>
2015-11-06 11:06:41 -0800

    update profile

--- src/updateprofile.php
+++ src/updateprofile.php
@@ -1,4 +1,6 @@
 <?php
+require_once('connect.php');
+
 function get_ProfileInfo($info)
 {
 	//echo $_SESSION['SESS_USERNAME'];
@@ -13,4 +15,36 @@ function get_ProfileInfo($info)
 		}
 	}
 }
+
+$newpicture = $newnickname = $newinterests = $newblogdesc = "";
+
+//text field
+if(isset($_POST['update_profile'])) {
+	
+	//session_start();
+	//session_id();
+	$_SESSION['SESS_LOGIN_ID'] = 2;
+	//set vaars to preexisting data from table
+	if (empty($_POST["picture_update"])) get_ProfileInfo('photo');
+	if (empty($_POST["nickname_update"])) get_ProfileInfo('nickname');
+	if (empty($_POST["interests_update"])) get_ProfileInfo('interests');
+	if (empty($_POST["blogdes_update"])) get_ProfileInfo('blogdesc');
+	//set new data
+	$newpicture = test_input($_POST["picture_update"]);
+	$newnickname = test_input($_POST["nickname_update"]);
+	$newinterests = test_input($_POST["interests_update"]);
+	$newblogdesc = test_input($_POST["blogdes_update"]);
+	
+	
+	//$sessionid = $_SESSION['SESS_LOGIN_ID'];
+	//sql update profile
+	$sql_addpost = "UPDATE profile SET nickname = '$newnickname', photo = '$newpicture', interests = '$newinterests', blogdesc = '$newblogdesc' WHERE profileID =  '$sessionid' ";
+	
+	echo $sql_addpost;
+	
+	//check_sql($sql_addpost, $conn);
+	//header("Location:index.php");
+	
+}
+
 ?>
