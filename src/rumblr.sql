226f2f4d5fc285f3f3344b33de3695581bdbc61c
dcsmitty <dcsmitty12@yahoo.com>
2015-11-12 23:18:20 -0800

    Fixed Database...again

--- src/rumblr.sql
+++ src/rumblr.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2015 at 07:01 AM
+-- Generation Time: Nov 13, 2015 at 08:17 AM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -72,6 +72,17 @@ CREATE TABLE `inbox` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `labels`
+--
+
+CREATE TABLE `labels` (
+  `User_ID` int(11) NOT NULL,
+  `Tag` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
@@ -81,6 +92,13 @@ CREATE TABLE `login` (
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`loginID`, `login`, `password`) VALUES
+(13, 'dsmit', '$2a$10$S0u.Gd3vIS.YqO0zJZvi5.f5fERQIJ1wVonD5YI6A31tTAYKLvHBe');
+
 -- --------------------------------------------------------
 
 --
@@ -89,6 +107,7 @@ CREATE TABLE `login` (
 
 CREATE TABLE `posts` (
   `postID` int(11) UNSIGNED NOT NULL,
+  `User_ID` int(11) UNSIGNED NOT NULL,
   `type` varchar(50) NOT NULL,
   `info` varchar(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -114,6 +133,13 @@ CREATE TABLE `profile` (
   `username` varchar(255) NOT NULL,
   `privacy` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`profileID`, `lname`, `fname`, `nickname`, `gender`, `birthday`, `profilecreation`, `photo`, `interests`, `blogdesc`, `username`, `privacy`) VALUES
+(13, 'smith', 'dan', NULL, 'Male', '2015-11-10', '2015-11-13 06:19:51', 0x687474703a2f2f733764342e7363656e65372e636f6d2f69732f696d6167652f5472656b42696379636c6550726f64756374732f64656661756c742d6e6f2d696d6167653f7769643d31343930266865693d31303830266669743d6669742c3126666d743d706e6726716c743d38302c31266f705f75736d3d302c302c302c3026696363456d6265643d30266267633d3234302c3234302c323430, NULL, NULL, 'dcsmitty', 'Open');
 
 --
 -- Indexes for dumped tables
@@ -145,12 +171,17 @@ ALTER TABLE `profile`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `postID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `profileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
