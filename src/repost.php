9373327c706987f19283432536a672faba0b8a19
dcsmitty <dcsmitty12@yahoo.com>
2015-12-04 12:59:40 -0800

    Tags now work with reposts

--- src/repost.php
+++ src/repost.php
@@ -9,7 +9,7 @@ require_once('connect.php');
 
 include 'posts.php';
 
-//follow field
+//repost field
 if(isset($_POST['repost_sub'])) {
 	$repostsuccess = true;
 	
@@ -23,6 +23,7 @@ if(isset($_POST['repost_sub'])) {
 			$type = $row['type'];
 			$info = $row['info'];
 			$repost = true;
+			
 			
 		}
 	}
@@ -39,6 +40,37 @@ if(isset($_POST['repost_sub'])) {
 		
 		check_sql($sql_addpost, $conn);
 	}
+	
+	$query = "SELECT * from posts";
+	$result = mysql_query($query);
+	while ($row = mysql_fetch_array($result))
+	{
+		if($row['User_ID'] == $_SESSION['SESS_LOGIN_ID'])
+		{
+			//session_destroy();
+			$lastpost = $row['postID'];
+			
+		}
+	}
+	
+	
+	$query2 = "SELECT * from tags";
+	$result2 = mysql_query($query2);
+	while ($row = mysql_fetch_array($result2))
+	{
+		if($row['Post_ID'] == $_POST['repost_enter'])
+		{
+			//session_destroy();
+			$tags = $row['Tag_label'];
+			$sql_addpost2 = "INSERT INTO tags (Post_ID, Tag_label)
+							VALUES ('$lastpost', '$tags')";
+
+			check_sql($sql_addpost2, $conn);
+			
+		}
+	}
+	
+	
 	header("Location:index.php");
 }
 
